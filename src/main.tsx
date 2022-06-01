923834a06ecbd856b3f415836a965cc726c1ab4a
suemor <3100825062@qq.com>
2022-06-01 10:23:14 +0800

    feat: hash route

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { NextUIProvider } from '@nextui-org/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import App from './App'
 import { RootStoreProvider } from './context/root-store'
 import './styles/index.css'
@@ -9,12 +9,12 @@ import 'animate.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <NextUIProvider>
         <RootStoreProvider>
           <App />
         </RootStoreProvider>
       </NextUIProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
