1181c4126468f153b9078b5f68d9f110cdcfb244
suemor <3100825062@qq.com>
2022-06-04 16:11:37 +0800

    chore: card button padding

--- src/pages/stack/index.tsx
+++ src/pages/stack/index.tsx
@@ -4,7 +4,7 @@ import { useStore } from '@/store'
 const Stack: React.FC = () => {
   const { userStore } = useStore()
   return (
-    <div className="flex flex-col overflow-y-auto p-2 animate__animated animate__fadeIn">
+    <div className="flex flex-col overflow-y-auto p-2 animate__animated animate__fadeIn md:pb-10">
       <StackProgrss userStore={userStore} />
     </div>
   )
