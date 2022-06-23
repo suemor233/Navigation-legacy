53e45468e12c2c187f114200fecb1df2b7cacfed
suemor <3100825062@qq.com>
2022-06-23 15:35:52 +0800

    fix: safari scroll

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -2,13 +2,15 @@ import { useStore } from '@/store'
 import { observer } from 'mobx-react'
 import AboutDetail from './detail'
 import AboutSimple from './simple'
-
+import { useMediaQuery } from 'react-responsive'
 const About: React.FC = () => {
   const { aboutStore } = useStore()
-
+  const isMobile = useMediaQuery({
+    query: '(max-width: 768px)'
+  })
   return (
     <>
-      <div className="px-10 pt-10 animate__animated animate__fadeIn h-full" style={{'overflow':'overlay'}}>
+      <div className="px-10 pt-10 animate__animated animate__fadeIn h-full" style={!isMobile ? {'overflow':'overlay'} : undefined}>
         {aboutStore.about && <AboutSimple aboutData={aboutStore.about} />}
         {aboutStore.about && <AboutDetail aboutData={aboutStore.about} />}
       </div>
