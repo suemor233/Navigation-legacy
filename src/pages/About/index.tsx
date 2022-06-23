f3f38e7a60a94ca76e6c5e619ead2845b22da0ba
suemor <3100825062@qq.com>
2022-06-23 14:32:39 +0800

    fix: scroll width

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,17 +1,15 @@
-
 import { useStore } from '@/store'
 import { observer } from 'mobx-react'
 import AboutDetail from './detail'
 import AboutSimple from './simple'
 
-
 const About: React.FC = () => {
-  const {aboutStore} = useStore()
+  const { aboutStore } = useStore()
 
   return (
     <>
-      <div className="px-10 pt-10 overflow-y-auto animate__animated animate__fadeIn h-full">
-        {aboutStore.about&& <AboutSimple aboutData={aboutStore.about} />}
+      <div className="px-10 pt-10 animate__animated animate__fadeIn h-full" style={{'overflow':'overlay'}}>
+        {aboutStore.about && <AboutSimple aboutData={aboutStore.about} />}
         {aboutStore.about && <AboutDetail aboutData={aboutStore.about} />}
       </div>
     </>
