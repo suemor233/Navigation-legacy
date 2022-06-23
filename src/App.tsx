4c3705221bfd42816a9e02d3c47815cb04b12cf3
suemor <3100825062@qq.com>
2022-06-23 15:27:53 +0800

    fix: mobile animation

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,12 @@ const App: React.FC = (): JSX.Element => {
     query: '(max-width: 768px)'
   })
 
+  
+  document.body.addEventListener('touchmove', function (e) {
+    e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
+  }, {passive: false}); //passive 参数不能省略，用来兼容ios和android
+  
+
   return (
     <>
       <div
--- src/pages/About/detail.tsx
+++ src/pages/About/detail.tsx
@@ -1,9 +1,8 @@
-
-import { AboutType } from '@/models/InformationConfigType';
-import UserStore from '@/store/user';
+import { AboutType } from '@/models/InformationConfigType'
+import UserStore from '@/store/user'
 import { Collapse, Text } from '@nextui-org/react'
-import { Fragment, useMemo } from 'react';
-import { useMediaQuery } from 'react-responsive';
+import { Fragment, useMemo } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const AboutDetail: React.FC<{ aboutData: AboutType[] }> = ({ aboutData }) => {
   const isMobile = useMediaQuery({
@@ -14,28 +13,27 @@ const AboutDetail: React.FC<{ aboutData: AboutType[] }> = ({ aboutData }) => {
     return aboutData
   }, [aboutData])
 
- return (
-   <>    
-     <Collapse.Group css={{ paddingLeft: '0px', marginTop: '2em' }}>
-          {ArrProgress.map((item, index) => {
-            return (
-              <Fragment key={index}>
-                
-                {index === 0 || isMobile ? (
-                  <Collapse title={item.key} expanded>
-                    <Text dangerouslySetInnerHTML={{ __html: item.value }}></Text>
-                  </Collapse>
-                ) : (
-                  <Collapse title={item.key} >
-                    <Text dangerouslySetInnerHTML={{ __html: item.value }}></Text>
-                  </Collapse>
-                )}
-              </Fragment>
-            )
-          })}
-        </Collapse.Group>
-   </> 
- ); 
-}; 
+  return (
+    <>
+      <Collapse.Group css={{ paddingLeft: '0px', marginTop: '2em' }}>
+        {ArrProgress.map((item, index) => {
+          return (
+            <Fragment key={index}>
+              {index === 0 || isMobile ? (
+                <Collapse title={item.key} expanded>
+                  <Text dangerouslySetInnerHTML={{ __html: item.value }}></Text>
+                </Collapse>
+              ) : (
+                <Collapse title={item.key}>
+                  <Text dangerouslySetInnerHTML={{ __html: item.value }}></Text>
+                </Collapse>
+              )}
+            </Fragment>
+          )
+        })}
+      </Collapse.Group>
+    </>
+  )
+}
 
-export default AboutDetail;
+export default AboutDetail
